606e889d2f84160f185ecf1899b8b1f6b6747e92
jiayun <jiayun@8caad68e-4ade-0310-88e9-b962042d36e4>
2005-08-16 12:29:28 +0000

    Extract OrderableFieldDialog from CompareToGenerator
    git-svn-id: http://svn.berlios.de/svnroot/repos/commons4e/trunk@45 8caad68e-4ade-0310-88e9-b962042d36e4

--- org.jiayun.commons4e/src/org/jiayun/commons4e/internal/lang/generators/CompareToGenerator.java
+++ org.jiayun.commons4e/src/org/jiayun/commons4e/internal/lang/generators/CompareToGenerator.java
@@ -15,20 +15,10 @@ import org.eclipse.jdt.core.compiler.InvalidInputException;
 import org.eclipse.jdt.ui.JavaUI;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.window.Window;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PartInitException;
-import org.jiayun.commons4e.internal.ui.dialogs.FieldDialog;
+import org.jiayun.commons4e.internal.ui.dialogs.OrderableFieldDialog;
 import org.jiayun.commons4e.internal.util.JavaUtils;
 
 /**
@@ -60,7 +50,7 @@ public final class CompareToGenerator implements ILangGenerator {
             excludedMethods.add(existingMethod);
         }
         try {
-            CompareToDialog dialog = new CompareToDialog(parentShell,
+            OrderableFieldDialog dialog = new OrderableFieldDialog(parentShell,
                     "Generate CompareTo Method", objectClass, JavaUtils
                             .getNonStaticNonCacheFields(objectClass), excludedMethods);
             int returnCode = dialog.open();
@@ -148,128 +138,6 @@ public final class CompareToGenerator implements ILangGenerator {
         content.append("}\n\n");
 
         return content.toString();
-    }
-
-    private static class CompareToDialog extends FieldDialog {
-
-        private Button upButton;
-
-        private Button downButton;
-
-        public CompareToDialog(Shell parentShell, String dialogTitle,
-                IType objectClass, IField[] fields, Set excludedMethods)
-                throws JavaModelException {
-            super(parentShell, dialogTitle, objectClass, fields,
-                    excludedMethods);
-        }
-
-        public void create() {
-            super.create();
-
-            Table fieldTable = fieldViewer.getTable();
-            fieldTable.addSelectionListener(new SelectionAdapter() {
-
-                public void widgetSelected(SelectionEvent e) {
-                    handleTableSelectionChanged();
-                }
-            });
-        }
-
-        private void handleTableSelectionChanged() {
-            Table fieldTable = fieldViewer.getTable();
-            TableItem[] items = fieldTable.getSelection();
-            boolean validSelection = items != null && items.length > 0;
-            boolean enableUp = validSelection;
-            boolean enableDown = validSelection;
-            if (validSelection) {
-                int indices[] = fieldTable.getSelectionIndices();
-                int max = fieldTable.getItemCount();
-                enableUp = indices[0] != 0;
-                enableDown = indices[indices.length - 1] < max - 1;
-            }
-            upButton.setEnabled(enableUp);
-            downButton.setEnabled(enableDown);
-        }
-
-        protected void addButtons(final Composite buttonComposite) {
-            super.addButtons(buttonComposite);
-
-            GridData data;
-            upButton = new Button(buttonComposite, SWT.PUSH);
-            upButton.setText("&Up");
-            upButton.setEnabled(false);
-            upButton.addListener(SWT.Selection, new Listener() {
-
-                public void handleEvent(Event event) {
-                    moveSelectionUp();
-                    handleTableSelectionChanged();
-                }
-            });
-            data = new GridData(GridData.FILL_HORIZONTAL);
-            upButton.setLayoutData(data);
-
-            downButton = new Button(buttonComposite, SWT.PUSH);
-            downButton.setText("Do&wn");
-            downButton.setEnabled(false);
-            downButton.addListener(SWT.Selection, new Listener() {
-
-                public void handleEvent(Event event) {
-                    moveSelectionDown();
-                    handleTableSelectionChanged();
-                }
-            });
-            data = new GridData(GridData.FILL_HORIZONTAL);
-            downButton.setLayoutData(data);
-        }
-
-        /**
-         * Move the current selection in the field list up.
-         */
-        private void moveSelectionUp() {
-            Table builderTable = fieldViewer.getTable();
-            int indices[] = builderTable.getSelectionIndices();
-            int newSelection[] = new int[indices.length];
-            for (int i = 0; i < indices.length; i++) {
-                int index = indices[i];
-                if (index > 0) {
-                    move(builderTable.getItem(index), index - 1);
-                    newSelection[i] = index - 1;
-                }
-            }
-            builderTable.setSelection(newSelection);
-        }
-
-        /**
-         * Move the current selection in the field list down.
-         */
-        private void moveSelectionDown() {
-            Table builderTable = fieldViewer.getTable();
-            int indices[] = builderTable.getSelectionIndices();
-            if (indices.length < 1) {
-                return;
-            }
-            int newSelection[] = new int[indices.length];
-            int max = builderTable.getItemCount() - 1;
-            for (int i = indices.length - 1; i >= 0; i--) {
-                int index = indices[i];
-                if (index < max) {
-                    move(builderTable.getItem(index), index + 1);
-                    newSelection[i] = index + 1;
-                }
-            }
-            builderTable.setSelection(newSelection);
-        }
-
-        /**
-         * Moves an entry in the field table to the given index.
-         */
-        private void move(TableItem item, int index) {
-            Object data = item.getData();
-            boolean checked = fieldViewer.getChecked(data);
-            item.dispose();
-            fieldViewer.insert(data, index);
-            fieldViewer.setChecked(data, checked);
-        }
     }
 
 }
